2642c6b355fbf485db68f2ed347b565faa106f48
Carol <86276520+carolmeraz@users.noreply.github.com>
2021-10-31 13:49:42 -0700

    Add files via upload

--- meraz.carolina/parts/meta.php
+++ meraz.carolina/parts/meta.php
@@ -1,5 +1,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+
+<base href="http://carolme.art/aau/wnm608/meraz.carolina/">
+
 	<link rel="stylesheet" href="lib/css/styleguide.css">
 	<link rel="stylesheet" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" href="css/storetheme.css">
